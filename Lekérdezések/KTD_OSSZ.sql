a13511262467a3b376a813ac16bdc6c9579214a0
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2023-12-01 11:55:59 +0100

    updt

---
+++ Lekérdezések/KTD_OSSZ.sql
@@ -0,0 +1,30 @@
+use [db_name] -- Csak T-SQL-ből futtatáshoz kell
+
+declare @cikkid int = null -- paraméter esetén: Integer, Semmi, Listából választható, cikk, cikkid, cikkszam
+declare @td_fotipus int = null -- paraméter esetén: Integer, Radio, üres, csomany, csomanyid, KTD;Hulladék kezelés;NETA;EPR
+
+select
+	lek_ktd.CIKKID,
+	SUM(lek_ktd.MENNY * lek_ktd.MESZORZO * lek_ktd.AR) ERTEK
+from
+	(
+		select
+			CIKKCSOM.CIKKID CIKKID,
+			CIKKCSOM.MENNY MENNY,
+			(
+				select top 1 CSOMANYAR.AR from CSOMANYAR with (nolock)
+				where CSOMANYAR.CSOMANYID = CSOMANY.CSOMANYID and convert(date, CSOMANYAR.DATUMTOL) <= GETDATE()
+				order by CSOMANYAR.CSOMANYARID desc
+			) AR,
+			CIKKME.MESZORZO MESZORZO
+		from
+			CIKKCSOM with (nolock)
+			join CSOMANY with (nolock) on CIKKCSOM.CSOMANYID = CSOMANY.CSOMANYID
+			left join CIKKME with (nolock) on CIKKCSOM.CIKKID = CIKKME.CIKKID and CIKKME.ME = CIKKCSOM.CSOMME
+		where
+			CIKKCSOM.CIKKID = ISNULL(@cikkid, CIKKCSOM.CIKKID)
+			and CSOMANY.TD_FOTIPUS = ISNULL(@td_fotipus, CSOMANY.TD_FOTIPUS)
+	) lek_ktd
+	join CIKK with (nolock) on lek_ktd.CIKKID = CIKK.CIKKID
+group by lek_ktd.CIKKID
+;
---
+++ README.md
@@ -0,0 +1,10 @@
+# Hasznos SQL részletek az Octopus 8 ERP-hez
+
+A paraméter ajánlás az alábbi sorrendben dolgozik:
+    ```Paraméter típusa, Paraméter altípus, Opciók, Táblanév, Mezőnév, Eredménymező / Rádió mező```
+
+## Lekérdezések
+
+### KTD_OSSZ
+Cikkentként visszaadja a 1 Készl. ME.-re a KTD értéket.
+szűrhető: cikkid, td_főtípus
